91f81c0e1ad4b769866584923f25cb24bc3653bf
Taher Moosa <lilt@Tahers-MBP.hsd1.il.comcast.net>
2023-09-11 16:18:09 -0500

    New.jsx

---
+++ views/New.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class New extends Component {
+  render() {
+    return (
+      <div>
+        <form action="/pokemon" method="post">
+            <h1 style={{textAlign: 'center', fontWeight: 'bolder'}}>Create a New Pokemon Here</h1>
+            Pokemon Name: <input type='text' name='name'/><br />
+            Pokemon Pic: <input type='text' name='img'/><br />
+            <input type='submit' />
+        </form>
+      </div>
+    )
+  }
+}
